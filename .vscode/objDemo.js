4d1297e1cc0425613d3be3ae207914b484a835e3
Cheng Liang (Louis) <madfoxfoxy@gmail.com>
2021-06-30 10:27:46 +0800

    update

---
+++ .vscode/objDemo.js
@@ -0,0 +1,17 @@
+let myObj;
+
+myObj = {
+    a: 19,
+    b: "dick"
+}
+
+console.log(myObj);
+
+myObj.a = 15;
+
+myObj.c = true;
+
+delete myObj.b
+
+console.log(myObj);
+
--- dick.js
+++ dick.js
@@ -1,1 +1,26 @@
-console.log("pussy");
+// console.log("pussy");
+
+// var a;
+
+// let myNum = 10;
+
+// console.log(myNum);
+
+// const myObj = {dick: 3};
+// console.log(myObj);
+
+let x = 10;
+let y = true
+let myObj = {
+    a : x,
+    b : y
+}
+x = 2 * x;
+let anoObj = {
+    newA : x,
+    b : y,
+    c : myObj
+}
+
+console.log(myObj)
+console.log(anoObj)
